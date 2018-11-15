cdcb415c5da0add0f03ce6e9a3bb1622ac97b0f7
LFMcrae <659457679@qq.com>
2018-11-15 14:20:46 +0800

    陶羽佳于 2018/11/15

--- app/src/main/java/com/nxnd/travelnote/fragment/EditFragment.java
+++ app/src/main/java/com/nxnd/travelnote/fragment/EditFragment.java
@@ -1,17 +1,21 @@
 package com.nxnd.travelnote.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.nxnd.travelnote.activity.MainActivity;
 import com.qmuiteam.qmui.widget.QMUIViewPager;
 import com.squareup.okhttp.Request;
 import com.nxnd.travelnote.R;
@@ -22,31 +26,31 @@ import com.nxnd.travelnote.listener.RecyclerItemClickListener;
 import com.nxnd.travelnote.model.RaidersModel;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
-
 import org.json.JSONArray;
 import org.json.JSONException;
-
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by linSir on 17/3/11.攻略
- */
+import butterknife.ButterKnife;
+
 public class EditFragment extends Fragment {
     private ImageView imageView;
     private RecyclerView recyclerView;
     private RaidersAdapter adapter;
     private LinearLayoutManager linearLayoutManager;
     private List<RaidersModel> mLists;
+    private String[]mCustomItems=new String[]{"本地相册","相机拍照"};
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit, container, false);
+        ButterKnife.bind(this,view);
         imageView = view.findViewById(R.id.imageView);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
 
             }
         });
